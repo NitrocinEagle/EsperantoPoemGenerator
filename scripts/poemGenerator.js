a44144029d09e3cb46c98803e6947185c1f50fb8
pc-mist <mist_sx@mail.ru>
2014-12-18 06:02:32 +0600

    Update poemGenerator.js

--- scripts/poemGenerator.js
+++ scripts/poemGenerator.js
@@ -14,9 +14,44 @@ function getLastMorpheme(strTemplate)
 	}
 	return lastMorpheme;
 }
+function generateSentences(sent1, sent2)
+{
+
+}
 function lol(templatesURL, form)
 {
 /*
+ * 
+{
+   "morpheme": "Noun",
+   "word": ""
+},
+{
+   "morpheme": "Verb",
+   "word": ""
+},
+{
+   "morpheme": "Adjective",
+   "word": ""
+},
+{
+   "morpheme": "Preposite",
+   "word": ""
+},
+{
+   "morpheme": "Participle",
+   "word": ""
+},
+{
+   "morpheme": "Pronoun",
+   "word": ""
+},
+{
+   "morpheme": "Adverb	",
+   "word": ""
+}
+]
+
 Функция, котороя берет четыре шаблона с рифмой ВААВ и подставляет в них слова
 Первый шаблон берется любым. Третий берется такой, у которого последняя морфема та же, что и у первого. Второй шаблон берется любой, у которого на конце морфема, отличающаяся от морфемы на конце первого и третьего шаблонов. Четвертый шаблон с такой же морфемой на конце, что и у второго
 */	
@@ -25,6 +60,7 @@ function lol(templatesURL, form)
 	request.send(null);
 	var JSON_object = JSON.parse(request.responseText);
 	var templates = new Array(4);
+	//document.form.sentences.value += JSON_object[Math.floor(Math.random()*JSON_object.length)].template;
 	templates[0] = JSON_object[Math.floor(Math.random()*JSON_object.length)].template;
 	templates[1] = JSON_object[Math.floor(Math.random()*JSON_object.length)].template;
 	do 
@@ -38,10 +74,50 @@ function lol(templatesURL, form)
 		templates[3] = JSON_object[Math.floor(Math.random()*JSON_object.length)].template;
 		if (getLastMorpheme(templates[1]) == getLastMorpheme(templates[3]))
 			break;
-	} while (true);
+	} while (true);	
 	
+	for (k = 0; k < templates.length; k++)
+	{
+		var str = templates[k], mas = [], j = 0;
+		for (i = 0; i < str.length; i++) 
+		{
+			if (str[i] == " ")
+			{ 
+				j++; 
+				continue; 
+			}
+			else 
+			{
+			mas[j] ? mas[j] += str[i] : mas[j] = str[i];
+			}
+		}		
+		for (i = 0; i < mas.length; i++)
+		{
+			var dictonaryURL = "";
+			if (mas[i] == "Verb")
+			{
+				dictonaryURL = "./Poem_Generator_files/Dictonary_Verbs.json";
+			}
+			else if (mas[i] == "Noun")
+			{
+				dictonaryURL = "./Poem_Generator_files/Dictonary_Nouns.json";
+			}
+			else if (mas[i] == "Adjective")
+			{
+				dictonaryURL = "./Poem_Generator_files/Dictonary_Adjectives.json";
+			}
+			var request = new XMLHttpRequest();
+			request.open("GET", dictonaryURL, false);
+			request.send(null);
+			var JSON_object = JSON.parse(request.responseText);	
+			document.form.sentences.value += JSON_object[Math.floor(Math.random()*JSON_object.length)].word + " ";			
+		}
+		document.form.sentences.value += "\n";
+	}
+
+		
 	for (i = 0; i < templates.length; i++)
-	{		
+	{
 		document.form.sentences.value += i + "-й шаблон - " + templates[i] + "\n";
-	}	
+	}
 }
